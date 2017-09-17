8984b683af113fa1736bdb4a8c5481dc76378ccf
cfstiff <cfstiff@gmail.com>
2017-09-17 15:56:16 -0700

    Working log-in page

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,18 @@
+class SessionsController < ApplicationController
+  def new
+  end
+
+  def create
+  	user = User.find_by(username: params[:session][:username].downcase)
+  	if user && user.authenticate(params[:session][:password])
+  		log_in user
+  		redirect_to ('/')
+  	else
+  		flash.now[:danger] = 'Invalid username/password combination'
+ 		render 'new' 	
+  	end
+  end
+
+  def destroy
+  end
+end
