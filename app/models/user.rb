c4e8e1822687bacb7e51a1f9bcbb359b89ac632e
cfstiff <cfstiff@gmail.com>
2017-09-17 08:37:16 -0700

    Finished setting up database to include unique usernames, email, and password

---
+++ app/models/user.rb
@@ -0,0 +1,14 @@
+class User < ApplicationRecord
+	# convert username to lowercase before saving
+	before_save { self.username = username.downcase }
+	# make sure the name and email fields 
+	# are filled in correctly
+	validates :username, presence: true, uniqueness: { case_sensitive: false }
+	# regular expression detailing the correct format for an email
+	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+	validates :email, presence: true, format: {with: VALID_EMAIL_REGEX }
+
+	has_secure_password
+	# require password be at least length 6
+	validates :password, presence:true, length: { minimum: 6 }
+end
