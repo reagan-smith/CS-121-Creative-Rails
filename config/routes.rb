0c7e4d2560a324e8638228d27b0fbe7901b08547
reagan-smith <reaganm.smith@yahoo.com>
2017-09-22 11:05:30 -0700

    bootstrap works, redirected paths

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
 
-  root 'application#mainPage'
+  root 'todos#index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :todos
