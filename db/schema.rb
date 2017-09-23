18f75fcc8bc991ce0374ee54e8889ec8449f6509
Maeve Murphy <maevemurphy530@gmail.com>
2017-09-23 14:57:01 -0700

    added Gemfile.lock and db/schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -19,14 +19,5 @@ ActiveRecord::Schema.define(version: 20170919192323) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
-    
-  create_table "Users", force: :cascade do |t|
-    t.string "username"
-    t.string "email"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.string "password_digest"
-    t.index ["username"], name: "index_users_on_username", unique: true
-  end
 
 end
